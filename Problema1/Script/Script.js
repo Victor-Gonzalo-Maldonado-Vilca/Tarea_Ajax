e6dde62f3f35378523663c5d21156c5f51455bd1
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 17:25:45 -0500

    Preparar datos de fechas y valores para la gráfica

--- Problema1/Script/Script.js
+++ Problema1/Script/Script.js
@@ -6,12 +6,15 @@ document.getElementById("comparasion").addEventListener("submit", function (even
   const region1 = formData.get('region1');
   const region2 = formData.get('region2');
   
-  fetch("http://localhost/Ajax/JASON/data.json")
+  fetch("http://localhost/nuevo/JASON/data.json")
     .then(response => response.json())
     .then(data => {
       //Solicitando informacion del archivo data.json 
       const datosRegion1 = data.find(region => region.region === region1);
       const datosRegion2 = data.find(region => region.region === region2);
 
+      const fechas = datosRegion1.confirmed.map(entry => entry.date);
+      const valoresRegion1 = datosRegion1.confirmed.map(entry => parseInt(entry.value));
+      const valoresRegion2 = datosRegion2.confirmed.map(entry => parseInt(entry.value));
     })
 });
