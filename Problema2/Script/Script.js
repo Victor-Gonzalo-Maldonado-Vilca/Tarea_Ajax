99b3814e6a4a5ddebbea9706a7cbeb0574c2cd86
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 18:34:37 -0500

    Agregar función para generar colores aleatorios para el gráfico

--- Problema2/Script/Script.js
+++ Problema2/Script/Script.js
@@ -35,6 +35,15 @@ document.getElementById("comparasion").addEventListener("submit", function (even
           backgroundColor: 'rgba(0, 0, 255, 0.1)'
         };
       });
-
     })
 });
+
+// Función para obtener un color aleatorio
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
