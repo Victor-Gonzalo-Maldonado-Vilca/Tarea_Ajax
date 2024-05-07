4e21eb765d9e0e574fc1425635b5d68f099296d1
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 18:12:25 -0500

    Extraer la primera región válida y obtener sus fechas

--- Problema2/Script/Script.js
+++ Problema2/Script/Script.js
@@ -3,4 +3,17 @@ document.getElementById("comparasion").addEventListener("submit", function (even
 
   fetch("http://localhost/Ajax/JSON/data.json")
     .then(response => response.json())
+    .then(data => {
+      //extrayendo primera región
+      const primeraRegion = data.find(region => region.region !== 'Lima' && region.region !== 'Callao');
+
+      // Verificar que se encontró la primera región
+      if (!primeraRegion) {
+        console.error('No se encontró la primera región con las fechas necesarias para el gráfico.');
+        return;
+      }
+
+      // Obtener las fechas de la primera región
+      const fechas = primeraRegion.confirmed.map(entry => entry.date);
+    })
 });
