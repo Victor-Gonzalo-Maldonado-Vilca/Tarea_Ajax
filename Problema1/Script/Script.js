a14ed2cc71a524d95c564c7cc7e95660aad8a985
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 17:15:41 -0500

    Agregando eventListener para el formulario y extracción de datos

---
+++ Problema1/Script/Script.js
@@ -0,0 +1,9 @@
+let myChart;
+document.getElementById("comparasion").addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  //Extraccion de datos del formulario
+  const formData = new FormData(this);
+  const region1 = formData.get('region1');
+  const region2 = formData.get('region2');
+  
+}
