e4c3f9072ee73909f97fd7d4c6dfe23c52e473f6
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 17:37:14 -0500

    Actualizar o crear gráfica con los datos de las regiones seleccionadas

--- Problema1/Script/Script.js
+++ Problema1/Script/Script.js
@@ -6,7 +6,7 @@ document.getElementById("comparasion").addEventListener("submit", function (even
   const region1 = formData.get('region1');
   const region2 = formData.get('region2');
   
-  fetch("http://localhost/nuevo/JASON/data.json")
+  fetch("http://localhost/Ajax/JASON/data.json")
     .then(response => response.json())
     .then(data => {
       //Solicitando informacion del archivo data.json 
@@ -16,5 +16,41 @@ document.getElementById("comparasion").addEventListener("submit", function (even
       const fechas = datosRegion1.confirmed.map(entry => entry.date);
       const valoresRegion1 = datosRegion1.confirmed.map(entry => parseInt(entry.value));
       const valoresRegion2 = datosRegion2.confirmed.map(entry => parseInt(entry.value));
+      if (myChart) {
+        //Si el archivo ya fue creado. 
+        myChart.data.labels = fechas;
+        myChart.data.datasets[0].label = region1;
+        myChart.data.datasets[0].data = valoresRegion1;
+        myChart.data.datasets[1].label = region2;
+        myChart.data.datasets[1].data = valoresRegion2;
+        myChart.update();
+      } else {
+        //caso contrario la crea
+        const ctx = document.getElementById('grafica').getContext('2d');
+        myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: fechas,
+            datasets: [
+              {
+                label: region1,
+                data: valoresRegion1,
+                borderColor: 'blue',
+                backgroundColor: 'rgba(0, 0, 255, 0.1)'
+              },
+              {
+                label: region2,
+                data: valoresRegion2,
+                borderColor: 'green',
+                backgroundColor: 'rgba(0, 255, 0, 0.1)'
+              }
+            ]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false
+          }
+        });
+      }
     })
 });
