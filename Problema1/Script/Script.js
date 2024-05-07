7599254d97522ceb47fbf98818e18a0bdee71c98
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-07 17:20:45 -0500

    Obtener datos del archivo JSON para las regiones seleccionadas

--- Problema1/Script/Script.js
+++ Problema1/Script/Script.js
@@ -6,4 +6,12 @@ document.getElementById("comparasion").addEventListener("submit", function (even
   const region1 = formData.get('region1');
   const region2 = formData.get('region2');
   
+  fetch("http://localhost/Ajax/JASON/data.json")
+    .then(response => response.json())
+    .then(data => {
+      //Solicitando informacion del archivo data.json 
+      const datosRegion1 = data.find(region => region.region === region1);
+      const datosRegion2 = data.find(region => region.region === region2);
+
+    })
 });
